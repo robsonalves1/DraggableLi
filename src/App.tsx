2e6085a8b68adf8ba09428b989a7a900e3a10f69
Robson Alves <robsonalvesparreiras@gmail.com>
2023-01-20 15:32:12 -0300

    I added a few comments in the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { useDrag } from '@use-gesture/react';
-import { useRef } from 'react';
+import { useDrag } from '@use-gesture/react'; /* import use-gesture/react to enable draggable items */
+import { useRef } from 'react'; /* import useRef to handle with mutable elements */
 import bikeRn from './img/project-bikeRn.png';
-import './App.css';
 import './Style.scss';
 
 function App() {
-  const slideRef: any = useRef(null);
+  const slideRef: any = useRef(null); /* create a constant to useRef */
 
   const bind = useDrag(({ offset: [x] }) => {
-    slideRef.current.style.transform = `translateX(${x}px)`
-  });
+    slideRef.current.style.transform = `translateX(${x}px)` /* change the x coordinate of current element */
+  }); /* create a const to useDrag*/
 
   return (
     <div
       className="slide-container"
       ref={slideRef} {...bind()}
-    >
+    > 
       <h1>This an Example of draggable li horizontally</h1>
 
       <ul>
